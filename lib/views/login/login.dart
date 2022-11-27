30fcea0f431e5423c0a1ddcec18fb43dfdb6bb8c
Martin Garcia <martinxf22@gmail.com>
2022-11-26 18:54:50 -0700

    Se está impelmentando el dashboard para responsable

--- lib/views/login/login.dart
+++ lib/views/login/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:sfaef/views/dashboard/dashboardResponsable.dart';
+
+import '../utils/utils.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
@@ -8,17 +9,9 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFF004990),
-        title: const Text(
-            'Sistema de Formulación y Aprobación de Eventos Formativos (SFAEF)',
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-                fontWeight: FontWeight.w200)),
-      ),
+      appBar: appBarTitle(),
       body: SingleChildScrollView(
-        child: Container(
+        child: SizedBox(
           width: double.infinity,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -32,29 +25,14 @@ class Login extends StatelessWidget {
                     width: double.infinity,
                     color: Colors.white,
                     height: 600,
-                    margin: const EdgeInsets.only(top: 30),
+                    padding: const EdgeInsets.only(top: 30),
                     child: Center(
                       child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Text(
-                                  textAlign: TextAlign.start,
+                            tituloDecorado(
+                              titulo:
                                   "PROCESO DE SOLICITUD DE EVENTOS FORMATIVOS",
-                                  style: TextStyle(
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.w500,
-                                      color: Color(0xFF004990)),
-                                ),
-                                //LINE ORANGE IN START
-                                Container(
-                                  width: 63,
-                                  height: 3,
-                                  color: Colors.orange,
-                                ),
-                              ],
                             ),
                             //container with F7F7F7 background color
                             const SizedBox(
@@ -106,7 +84,13 @@ class Login extends StatelessWidget {
                                               borderRadius:
                                                   BorderRadius.circular(16),
                                             )),
-                                        onPressed: () {},
+                                        onPressed: () {
+                                          Navigator.pushReplacement(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      const DashboardResponsable()));
+                                        },
                                         child: const Text(
                                           "Iniciar Sesión",
                                           style: TextStyle(
@@ -116,7 +100,7 @@ class Login extends StatelessWidget {
                                           ),
                                         )),
                                   ),
-                                  Text(
+                                  const Text(
                                     '* En caso de contar con un evento formativo aprobado previamente por el SFAEF favor de iniciar sesión',
                                     textAlign: TextAlign.center,
                                     style: TextStyle(
@@ -137,7 +121,7 @@ class Login extends StatelessWidget {
                     child: Center(
                       child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
+                          children: const [
                             Text('2022 |',
                                 style: TextStyle(
                                     color: Colors.white, fontSize: 16)),
@@ -163,42 +147,6 @@ class Login extends StatelessWidget {
       ),
     );
   }
-
-  SizedBox header() {
-    return SizedBox(
-      height: 144,
-      child: Stack(
-        children: [
-          Container(
-            height: 144,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(
-                  'assets/images/headerlogo.png',
-                ),
-                opacity: 0.15,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          // Container(
-          //   height: 300,
-          //   decoration: BoxDecoration(
-          //     color: Colors.white.withOpacity(0.70),
-          //   ),
-          // ),
-          Center(
-            child: Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/images/logo.png'),
-                  fit: BoxFit.contain,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
 }
+
+
